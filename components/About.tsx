aa34d62fef137a94bad0615dd0c713fd840cd897
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-01 15:14:51 +0100

    Added link to a

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,5 @@
 const links = [
-    { name: 'Frontend Development', href: '#' , description:"I build simple static and dynamic websites, sleek and attractive landing pages, e-commerce websites & other interesting frontend UI(User Interface). I build fast and efficient Single Page Applications using React & NEXTjs."},
+    { name: 'Frontend Development', href: '#' , description:"I build simple static and dynamic websites, sleek and attractive landing pages, e-commerce websites & other interesting frontend UI(User Interface). I also develop very efficient Single Page Applications using React & NEXTjs."},
     { name: 'Backend Development', href: '#' , description:"I build and maintain fast, scalable and efficient models used for building simple to robust backend systems infrastructure and APIS. Scripting of cache controllers and application load balancers to help balance traffic on servers."},
     { name: 'Scripting & Web3', href: '' , description:"I write scripts that scrape the internet, perform custom tasks as instructed and build bots to perform any task needed. This range from web scrapers & crawlers to custom data science tools written in python."},
     { name: 'Cloud Architecture', href:'', description: 'I make use of AWS to host & create cloud based infrastructure, i build secure and scalable cloud based application logic. I scale applications from regular shared hosting to cloud based hosting helping to save resources and build an efficient, modern hosting process.' },
@@ -15,17 +15,16 @@ const links = [
           <div className="mx-auto max-w-2xl lg:mx-0">
 
             <p className="mt-3 text-sm leading-6 text-gray-800">
-            My name is <b className="text-bold text-underline">Enimofe Odujirin</b> and i am a lover of innovation & technology, I am a Fullstack Developer & Cloud Architect based in Lagos, Nigeria. I am skilled in working with wide range of front and back-end technologies to produce and evaluate functional and efficient code, applications. Currently i am experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive data driven applications.
+            My name is <b className="text-bold text-underline">Enimofe Odujirin</b> a lover of innovation & technology, a Fullstack Developer & Cloud Architect based in Lagos, Nigeria. Very skilled in working with wide range of front and back-end technologies to produce and evaluate functional and efficient code, applications. Currently i am currentling experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive data driven applications.
 
 
             </p> <br /><br />
            <div className="mx-auto ">
            <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">About me</h2>
             <p className="mt-5 text-sm leading-6 text-gray-600">
-            Enimofe Odujirin is a skilled Fullstack web developer, Cloud Architect and Entrepreneur with 5 years of experience. I love innovation and 
-            technology and enjoy solving complex problems by creating functional applications. 
-            I started with Python and moved on to create dynamic websites using PHP, Javascript, and Python.
-             I have also worked with popular web frameworks such as Flask and Django, which use a Model-View-Controller approach. 
+            Enimofe Odujirin is a skilled Fullstack web developer, Cloud Architect and Entrepreneur with 5 years of experience. I love enjoy solving complex problems by creating functional applications. 
+            I started with Python and moved on to creating dynamic websites using PHP, Javascript, and Python as my backend language.
+             I also work with popular web frameworks such as Flask and Django, which use a Model-View-Controller approach. 
              My expertise includes <b className="text-bold text-underline"> Python, Flask, Django, ReactJS, Postgresql, tailwindcss, chakraui, and bootstrap,</b> among others. 
              I am a strong candidate for any project or position requiring a Fullstack web developer who can create innovative solutions.      </p> 
            </div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -54,7 +54,7 @@ export default function NavBar() {
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
+                <Link href={item.href}>{item.name}</Link>
               </a>
             ))}
           </div>
