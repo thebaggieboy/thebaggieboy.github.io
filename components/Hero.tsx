196ed62bbdd857bf8dfa23eff42344b881d217a1
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-01 23:32:15 +0100

    Added about link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
             <div className=" sm:mb-8 sm:flex sm:justify-center">
               <motion.div initial={{y:-250}} animate={{y:-10}} transition={{delay:0.4, duration:0.8}} className="relative rounded-full px-3 p-2 text-sm text-center leading-5 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                Learn more
-                <a href="#about" className="font-semibold text-indigo-600 pl-2">
+                <a href="#about"  className="font-semibold text-indigo-600 pl-2">
                   <span className="absolute inset-0 text-center" aria-hidden="true" />
                    about me <span aria-hidden="true">&rarr;</span>
                 </a>
@@ -26,10 +26,10 @@ export default function Hero() {
               <motion.h1 initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.2, duration:0.6}} className="text-4xl  mt-1 font-bold tracking-tight text-gray-900 sm:text-6xl">
               Software Engineer, Founder & Entrepreneur
               </motion.h1>
-              <p className="mt-6 text-sm leading-6 text-gray-600">
+              <motion.p initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.2, duration:0.8}} className="mt-6 text-sm leading-6 text-gray-600">
               I am an innovative Software Engineer adept at working with customers, team members and project leaders to accomplish and update tasks according to timelines.
              
-              </p> <br/>
+              </motion.p> <br/>
               <motion.a href="../Resume.pdf" initial={{opacity:0}} animate={{opacity:1}} whileHover={{scale:1.5}} transition={{delay:0.8, duration:1.2}} className='inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700'>Check out my resume</motion.a>
               
             </div>
