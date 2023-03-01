766f71d4050fc213596b159eac6daddf79ce11aa
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-01 02:35:58 +0100

    s

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -17,7 +17,6 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import Link from 'next/link'
 
 const navigation = [
   { name: 'Home', href: '/' },
