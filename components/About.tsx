71e8e72ae8997842780bdc150fc533023c2e6699
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-02 06:29:40 +0100

    Installed react intersctiion observer

--- components/About.tsx
+++ components/About.tsx
@@ -8,40 +8,42 @@ const links = [
   
   export default function About() {
     return (
-      <div className="relative isolate overflow-hidden">
+     <main className="">
+       <div className="relative  px-6 lg:px-8 isolate overflow-hidden">
       
-        <div className="mx-auto p-10 max-w-7xl px-6 lg:px-8">
-        <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">What i do ?</h2>
-          <div className="mx-auto max-w-2xl lg:mx-0">
+      <div className="mx-auto p-10 max-w-7xl px-6 lg:px-8">
+      <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">What i do ?</h2>
+        <div className="mx-auto max-w-2xl lg:mx-0">
 
-            <p className="mt-3 text-sm leading-6 text-gray-800">
-            My name is <b className="text-bold text-underline">Enimofe Odujirin</b> a lover of innovation & technology, a fullstack developer & cloud architect with 4 ( four ) years of professional experience based in Lagos, Nigeria.  Currently i am experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive high traffic data driven applications.
+          <p className="mt-3 text-sm leading-6 text-gray-800">
+          My name is <b className="text-bold text-underline">Enimofe Odujirin</b> a lover of innovation & technology, a fullstack developer & cloud architect with 4 ( four ) years of professional experience based in Lagos, Nigeria.  Currently i am experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive high traffic data driven applications.
 
 
-            </p> <br /><br />
-           <div id="about" className="mx-auto ">
-           <h2 className="text-4xl font-bold tracking-tight pt-4 text-black sm:text-6xl">About me</h2>
-            <p className="mt-5 text-sm leading-6 text-gray-600">
-       I love enjoy solving complex problems by creating live functional applications. 
-            I started with Python and moved on to creating dynamic websites using PHP, Javascript, and Python as my backend language.
-             I also work with popular web frameworks such as Flask and Django, which use a Model-View-Controller approach. 
-             My expertise includes <b className="text-bold text-underline"> Python, Flask, Django, ReactJS, Postgresql, tailwindcss, chakraui, and bootstrap,</b> among others. 
-             I am a strong candidate for any project or position requiring a fullstack web developer who can create innovative solutions, as i am very skilled in working with wide range of front and back-end technologies to produce, evaluate efficient code and applications.      </p> 
-           </div>
-          </div><br/>
-          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-            <div className="grid grid-cols-1 gap-y-6 gap-x-6 leading-6 text-dark sm:grid-cols-2 md:flex lg:gap-x-10">
-              {links.map((link) => (
-                <a key={link.name}  href={link.href}>
-           <h1 className="font-bold">{link.name} <span aria-hidden="true">&rarr;</span></h1> <br/>
-                  <p className="text-gray-600 text-sm">{link.description}</p>
-                </a>
-              ))}
-             
-            </div>
+          </p> <br /><br />
+         <div id="about" className="mx-auto ">
+         <h2 className="text-3xl font-bold tracking-tight pt-4 text-black sm:text-6xl">About me</h2>
+          <p className="mt-5 text-sm leading-6 text-gray-600">
+     I love enjoy solving complex problems by creating live functional applications. 
+          I started with Python and moved on to creating dynamic websites using PHP, Javascript, and Python as my backend language.
+           I also work with popular web frameworks such as Flask and Django, which use a Model-View-Controller approach. 
+           My expertise includes <b className="text-bold text-underline"> Python, Flask, Django, ReactJS, Postgresql, tailwindcss, chakraui, and bootstrap,</b> among others. 
+           I am a strong candidate for any project or position requiring a fullstack web developer who can create innovative solutions, as i am very skilled in working with wide range of front and back-end technologies to produce, evaluate efficient code and applications.      </p> 
+         </div>
+        </div><br/>
+        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 gap-y-6 gap-x-6 leading-6 text-dark sm:grid-cols-2 md:flex lg:gap-x-10">
+            {links.map((link) => (
+              <a key={link.name}  href={link.href}>
+         <h1 className="font-bold">{link.name} <span aria-hidden="true">&rarr;</span></h1> <br/>
+                <p className="text-gray-600 text-sm">{link.description}</p>
+              </a>
+            ))}
+           
           </div>
         </div>
       </div>
+    </div>
+     </main>
     )
   }
   
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,17 +12,17 @@ export default function Hero() {
         <br /> <br />
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-10 sm:py-20 lg:py-20">
-            <div className=" sm:mb-8 sm:flex sm:justify-center">
-              <motion.div initial={{y:-250}} animate={{y:-10}} transition={{delay:0.4, duration:0.8}} className="relative rounded-full px-3 p-2 text-sm text-center leading-5 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div className=" sm:mb-8 ">
+              <motion.div initial={{y:-250}} animate={{y:-10}} transition={{delay:0.4, duration:0.8}} className="relative rounded-full px-3 p-2 text-sm  leading-5 text-gray-700 ring-1 ring-gray-400/10 hover:ring-gray-900/20">
                Learn more
                 <a href="#about"  className="font-semibold text-indigo-600 pl-2">
-                  <span className="absolute inset-0 text-center" aria-hidden="true" />
+                  <span className="absolute inset-0 " aria-hidden="true" />
                    about me <span aria-hidden="true">&rarr;</span>
                 </a>
               </motion.div>
 
             </div> 
-            <div className="text-center mx-auto p-5">
+            <div className=" mx-auto p-5">
               <motion.h1 initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.2, duration:0.6}} className="text-4xl  mt-1 font-bold tracking-tight text-gray-900 sm:text-6xl">
               Software Engineer, Founder & Entrepreneur
               </motion.h1>
@@ -30,7 +30,7 @@ export default function Hero() {
               I am an innovative Software Engineer adept at working with customers, team members and project leaders to accomplish and update tasks according to timelines.
              
               </motion.p> <br/>
-              <motion.a href="/Resume-Enimofe-Odujirin-2023.pdf" initial={{opacity:0}} animate={{opacity:1}} whileHover={{scale:1.5}} transition={{delay:0.8, duration:1.2}} className='inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700'>Check out my resume</motion.a>
+              <motion.a href="/Resume-Enimofe-Odujirin-2023.pdf" initial={{opacity:0}} animate={{opacity:1}} whileHover={{scale:1.5}} transition={{delay:0.8, duration:1.2}} className='inline-flex rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700'>Check out my resume</motion.a>
               
             </div>
           </div>
