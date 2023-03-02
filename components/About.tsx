ca4ed3c18bb7916a06abc089425a109b999ab83e
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-02 04:02:55 +0100

    about transition changes

--- components/About.tsx
+++ components/About.tsx
@@ -15,18 +15,18 @@ const links = [
           <div className="mx-auto max-w-2xl lg:mx-0">
 
             <p className="mt-3 text-sm leading-6 text-gray-800">
-            My name is <b className="text-bold text-underline">Enimofe Odujirin</b> a lover of innovation & technology, a fullstack developer & cloud architect with 4 ( four ) years of professional experience based in Lagos, Nigeria. Very skilled in working with wide range of front and back-end technologies to produce, evaluate efficient code and applications. Currently i am experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive data driven applications.
+            My name is <b className="text-bold text-underline">Enimofe Odujirin</b> a lover of innovation & technology, a fullstack developer & cloud architect with 4 ( four ) years of professional experience based in Lagos, Nigeria.  Currently i am experimenting with APIS and database models, looking towards improving my efficiency in building fast data-driven applications like geolocation systems and other intensive data driven applications.
 
 
             </p> <br /><br />
            <div id="about" className="mx-auto ">
            <h2 className="text-4xl font-bold tracking-tight pt-4 text-black sm:text-6xl">About me</h2>
             <p className="mt-5 text-sm leading-6 text-gray-600">
-       I love enjoy solving complex problems by creating functional applications. 
+       I love enjoy solving complex problems by creating live functional applications. 
             I started with Python and moved on to creating dynamic websites using PHP, Javascript, and Python as my backend language.
              I also work with popular web frameworks such as Flask and Django, which use a Model-View-Controller approach. 
              My expertise includes <b className="text-bold text-underline"> Python, Flask, Django, ReactJS, Postgresql, tailwindcss, chakraui, and bootstrap,</b> among others. 
-             I am a strong candidate for any project or position requiring a Fullstack web developer who can create innovative solutions.      </p> 
+             I am a strong candidate for any project or position requiring a fullstack web developer who can create innovative solutions, as i am very skilled in working with wide range of front and back-end technologies to produce, evaluate efficient code and applications.      </p> 
            </div>
           </div><br/>
           <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
