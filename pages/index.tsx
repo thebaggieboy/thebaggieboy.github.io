192e11db6de9628a28da9186b10ad157a4c390e0
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-05-19 11:25:37 +0100

    dd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
